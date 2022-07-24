c3eaab9e9996d7d68e26ff596f370a647dd7d30c
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-07-24 15:16:52 -0300

    Optimizacion del codigo

--- src/calendar/components/FabDelete.jsx
+++ src/calendar/components/FabDelete.jsx
@@ -1,19 +1,28 @@
-import { useCalendarStore, useUiStore } from '../../hooks';
+import { useAuthStore, useCalendarStore, useUiStore } from '../../hooks';
 
 export const FabDelete = () => {
-    const { startDeletingEvent, hasEventSelected } = useCalendarStore();
+    const { user } = useAuthStore();
+    const { startDeletingEvent, hasEventSelected, activeEvent } =
+        useCalendarStore();
     const { isDateModalOpen } = useUiStore();
 
     const handleDelete = () => {
         startDeletingEvent();
     };
 
+    const isMyEvent =
+        user.uid === activeEvent?.user._id ||
+        user.uid === activeEvent?.user.uid;
+
     return (
         <button
             className="btn btn-danger fab-danger"
             onClick={handleDelete}
             style={{
-                display: hasEventSelected && !isDateModalOpen ? '' : 'none',
+                display:
+                    hasEventSelected && !isDateModalOpen && isMyEvent
+                        ? ''
+                        : 'none',
             }}
         >
             <i className="fas fa-trash-alt"></i>
