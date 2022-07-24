ffc542bafedf531441e5f699b08fb52c2b7b9e9e
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-07-23 22:42:53 -0300

    Frontend deploy

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -81,9 +81,6 @@ export const CalendarModal = () => {
         if (formValues.title.length === 0) {
             return;
         }
-
-        console.log(formValues);
-        //Todo:
         await startSavingEvent(formValues);
         closeDateModal();
         setFormSubmitted(false);
